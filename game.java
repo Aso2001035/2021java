b07c60f25c053ea70c9833e7f6f8e51ba7948222
Aso2001035 <83050643+Aso2001035@users.noreply.github.com>
2021-07-30 16:15:42 +0900

    Create game.java

---
+++ game.java
@@ -0,0 +1,15 @@
+package last_challenge;
+import java.util.*;
+public class Game {
+    static final int mapH = 5;//エリアの縦の広さ
+    static final int mapW = 5;//エリアの横の広さ
+    static boolean map[][] = new boolean[mapH][mapW];//マップ
+    static Ship ships[] = new Ship[3];//船のインスタンス
+    static int turncount = 1;//ターン数
+    static int dropx;//爆弾を落とすX座標
+    static int dropy;//爆弾を落とすY座標
+
+    
+
+    
+}
